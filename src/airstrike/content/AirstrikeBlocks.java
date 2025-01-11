61cf43f3e5074f36fb357591d0bf44680fad05c1
Christoph Cernusca <cernusca.c22@htlwienwest.at>
2025-01-11 12:18:17 +0100

    Added Weapons Assemblers - Added Nuke Assembler - Added Precision Bomb Assembler

--- src/airstrike/content/AirstrikeBlocks.java
+++ src/airstrike/content/AirstrikeBlocks.java
@@ -10,7 +10,6 @@ import mindustry.gen.Sounds;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.production.GenericCrafter;
-import mindustry.world.meta.BlockGroup;
 import mindustry.world.meta.BuildVisibility;
 
 import static mindustry.type.ItemStack.with;
@@ -23,8 +22,13 @@ public class AirstrikeBlocks {
     public static Block smallSatelliteAssembler;
     public static Block mediumSatelliteAssembler;
     public static Block largeSatelliteAssembler;
+    public static Block nukeAssembler;
+    public static Block precisionBombAssembler;
 
     public static void load() {
+
+        // TODO: Balancing
+
         launcher = new Launcher("launcher") {{
             requirements(Category.effect, BuildVisibility.shown, with(Items.copper, 1));
             size = 5;
@@ -102,6 +106,37 @@ public class AirstrikeBlocks {
             consumePower(10f);
             consumeLiquid(Liquids.oil, 0.5f);
             outputItems = with(AirstrikeItems.largeSatellite, 1);
+
+            alwaysUnlocked = true;
+        }};
+
+        nukeAssembler = new GenericCrafter("nuke-assembler") {{
+            requirements(Category.effect, BuildVisibility.shown, with(Items.copper, 1));
+            size = 1;
+            ambientSound = Sounds.machine;
+            ambientSoundVolume = 0.1f;
+            craftTime = 1200f;
+            itemCapacity = 70;
+
+            consumeItems(with(Items.thorium, 70, Items.lead, 30, Items.blastCompound, 50, Items.phaseFabric, 10));
+            consumePower(10f);
+            consumeLiquid(Liquids.cryofluid, 0.1f);
+            outputItems = with(AirstrikeItems.nuke, 1);
+
+            alwaysUnlocked = true;
+        }};
+
+        precisionBombAssembler = new GenericCrafter("precision-bomb-assembler") {{
+            requirements(Category.effect, BuildVisibility.shown, with(Items.copper, 1));
+            size = 1;
+            ambientSound = Sounds.machine;
+            ambientSoundVolume = 0.1f;
+            craftTime = 120f;
+            itemCapacity = 30;
+
+            consumeItems(with(Items.titanium, 10, Items.blastCompound, 30));
+            consumePower(5f);
+            outputItems = with(AirstrikeItems.precisionBomb, 1);
 
             alwaysUnlocked = true;
         }};
